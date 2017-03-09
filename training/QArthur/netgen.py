bf2ee8e1fdcdd5380388b002e1c204378fee0e98
Benjamin Gorman <8076bgorman@gmail.com>
2017-03-09 15:45:24 +0000

    Add QArthur

---
+++ training/QArthur/netgen.py
@@ -0,0 +1,8 @@
+"""
+This script generates a neural network and serializes into the form that NeuralNetwork.as understands.
+"""
+
+NUM_INPUTS = 20
+HIDDEN_LAYER_SIZE = 64
+NUM_HIDDEN_LAYERS = 2
+NUM_OUTPUTS = 18
